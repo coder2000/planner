d42125a4ee796e4e4ead0a89a57e4af0e65dfd0e
Dieter <coder2000@gmail.com>
2010-04-11 02:07:13 -0500

    Refactored methods

--- lib/planner/acts_as_calendar_event.rb
+++ lib/planner/acts_as_calendar_event.rb
@@ -1,13 +1,13 @@
 module Planner
   module ActsAsCalendarEvent
-    def self.included(base)
-      base.extend Planner::ActsAsCalendarEvent::ClassMethods
+    def acts_as_calendar_event
+      self.extend Planner::ActsAsCalendarEvent::ClassMethods
     end
 
     module ClassMethods
-      def acts_as_calendar_event
-        include Planner::ActsAsCalendarEvent::InstanceMethods
-      end
+      # def acts_as_calendar_event
+      #   include Planner::ActsAsCalendarEvent::InstanceMethods
+      # end
 
       def get_events(current_month, first_day_of_week=0)
       end
